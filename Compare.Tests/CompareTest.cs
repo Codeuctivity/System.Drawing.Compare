683057ff61fc457942671adb1ccb4ce9ff5364b7
Stefan Seeland <168659+stesee@users.noreply.github.com>
2020-05-03 11:26:58 +0200

    Defined exception for os dependend things in tests

--- Compare.Tests/CompareTest.cs
+++ Compare.Tests/CompareTest.cs
@@ -1,5 +1,6 @@
 using Codeuctivity;
 using NUnit.Framework;
+using System.Runtime.InteropServices;
 
 namespace System.Drawing.CompareTestNunit
 {
@@ -19,19 +20,22 @@ namespace System.Drawing.CompareTestNunit
         }
 
         [Test]
-        [TestCase(jpgCalc0, pngCalc0, 443191, 2, 153760, 95.122615129543945d)]
-        [TestCase(jpgCalc1, pngCalc1, 441320, 2, 153802, 95.148598153967981d)]
-        [TestCase(pngCalc1, pngCalc1, 0, 0, 0, 0)]
-        [TestCase(jpgCalc1, jpgCalc1, 0, 0, 0, 0)]
-        [TestCase(jpgCalc0, jpgCalc1, 208886, 1, 2094, 1.2954393605701418d)]
-        [TestCase(pngCalc0, pngCalc1, 203027, 1, 681, 0.42129618173269651d)]
-        public void ShouldVerifyThatImagesAreSemiEqual(string pathPic1, string pathPic2, int expectedAbsoluteError, int expectedMeanError, int expectedPixelErrorCount, double expectedPixelErrorPercentage)
+        [TestCase(jpgCalc0, pngCalc0, 443191, 2, 153760, 95.122615129543945d, true)]
+        [TestCase(jpgCalc1, pngCalc1, 441320, 2, 153802, 95.148598153967981d, true)]
+        [TestCase(pngCalc1, pngCalc1, 0, 0, 0, 0, false)]
+        [TestCase(jpgCalc1, jpgCalc1, 0, 0, 0, 0, false)]
+        [TestCase(jpgCalc0, jpgCalc1, 208886, 1, 2094, 1.2954393605701418d, true)]
+        [TestCase(pngCalc0, pngCalc1, 203027, 1, 681, 0.42129618173269651d, false)]
+        public void ShouldVerifyThatImagesAreSemiEqual(string pathPic1, string pathPic2, int expectedAbsoluteError, int expectedMeanError, int expectedPixelErrorCount, double expectedPixelErrorPercentage, bool expectedValuesOsDependent)
         {
             var diff = Compare.CalcDiff(pathPic1, pathPic2);
-            Assert.That(diff.AbsoluteError, Is.EqualTo(expectedAbsoluteError), "AbsoluteError");
-            Assert.That(diff.MeanError, Is.EqualTo(expectedMeanError), "MeanError");
-            Assert.That(diff.PixelErrorCount, Is.EqualTo(expectedPixelErrorCount), "PixelErrorCount");
-            Assert.That(diff.PixelErrorPercentage, Is.EqualTo(expectedPixelErrorPercentage), "PixelErrorPercentage");
+            if (expectedValuesOsDependent && !RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                Assert.That(diff.AbsoluteError, Is.EqualTo(expectedAbsoluteError), "AbsoluteError");
+                Assert.That(diff.MeanError, Is.EqualTo(expectedMeanError), "MeanError");
+                Assert.That(diff.PixelErrorCount, Is.EqualTo(expectedPixelErrorCount), "PixelErrorCount");
+                Assert.That(diff.PixelErrorPercentage, Is.EqualTo(expectedPixelErrorPercentage), "PixelErrorPercentage");
+            }
         }
 
         [TestCase(pngCalc0, pngCalc1, 0, 0, 0, 0)]
